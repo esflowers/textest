720cceb950c886f8f1aa518dffc6c7907bb7b84c
esflowers <estrada.flsw@gmail.com>
2025-01-05 23:07:27 -0600

    add content for test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<script setup>
+  import BtnTheme from './components/buttons/BtnTheme.vue';
+  import BtnPrimary from './components/buttons/BtnPrimary.vue';
+</script>
+
 <template>
-    <main>
-      <h1 class="text-5xl text-green-500">Vue.js</h1>
-    </main>
+  <div class="flex items-center gap-5">
+    <BtnTheme />
+    <BtnPrimary text="Copy Tailwind" />
+  </div>
+  <main class="py-3 w-full h-1/2 flex items-center justify-center font-serif text-9xl text-center italic">
+    write here
+  </main>
 </template>
