9c4f8f06e1d0a786566afa034107b2204ee66566
esflowers <estrada.flsw@gmail.com>
2025-01-05 23:06:13 -0600

    create theme button

---
+++ src/components/buttons/BtnTheme.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import IconLight from '../icons/IconLight.vue';
+    import IconMoon from '../icons/IconMoon.vue';;
+
+    function changeTheme() {
+        document.documentElement.classList.toggle('dark');
+        const theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light'
+        localStorage.setItem('theme', theme)
+    }
+</script>
+
+<template>
+    <button @click="changeTheme" title="Change theme" class="p-1.5 rounded-lg text-zinc-800 bg-transparent transition duration-150 hover:text-black hover:bg-zinc-200/65 dark:text-zinc-300 dark:hover:text-white dark:hover:bg-zinc-900/65">
+        <span class="flex dark:hidden"><IconMoon :size="22" /></span>
+        <span class="hidden dark:block"><IconLight :size="23" /></span>
+    </button>
+</template>
