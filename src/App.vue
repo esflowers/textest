bbfe0b0044fc491c008b458730e2a7c4d2f74bd2
esflowers <estrada.flsw@gmail.com>
2025-01-08 23:17:18 -0600

    add first version of header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup>
-  import BtnTheme from './components/buttons/BtnTheme.vue';
-  import BtnPrimary from './components/buttons/BtnPrimary.vue';
+  import LayoutHeader from './layouts/LayoutHeader.vue';
 </script>
 
 <template>
-  <div class="flex items-center gap-5">
-    <BtnTheme />
-    <BtnPrimary text="Copy Tailwind" />
-  </div>
-  <main class="py-3 w-full h-1/2 flex items-center justify-center font-serif text-9xl text-center italic">
-    write here
+  <LayoutHeader />
+  <main class="py-3 w-full h-1/2  flex items-center justify-center font-serif text-9xl text-center italic">
+    write here Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro voluptatum possimus sint neque, asperiores nulla magnam consequuntur cupiditate odit dolore molestias eligendi hic praesentium laudantium quod dolores amet quam rerum!
   </main>
 </template>
---
+++ src/layouts/LayoutHeader.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import IconTextest from '@/components/icons/IconTextest.vue';
+    import BtnTheme from '@/components/buttons/BtnTheme.vue';
+    import BtnPrimary from '@/components/buttons/BtnPrimary.vue';
+</script>
+
+<template>
+    <header class="w-full h-fit fixed top-0 left-0 px-6 py-3 sm:px-8 sm:py-4 md:px-9 xl:px-12 2xl:py-5 flex items-center justify-between bg-white dark:bg-zinc-950">
+        <a href="/index.html">
+            <IconTextest :size="21" />
+        </a>
+        <div class="flex items-center justify-between gap-3">
+            <BtnTheme />
+            <BtnPrimary />
+        </div>
+    </header>
+</template>
