24e6ddb8af114ef38c52c93c1c2a4d7f73b06755
esflowers <estrada.flsw@gmail.com>
2025-01-09 15:38:21 -0600

    edit colors btn theme

--- src/components/buttons/BtnTheme.vue
+++ src/components/buttons/BtnTheme.vue
@@ -10,8 +10,8 @@
 </script>
 
 <template>
-    <button @click="changeTheme" title="Change theme" class="p-1.5 rounded-lg text-zinc-800 bg-transparent transition duration-150 hover:text-black hover:bg-zinc-200/65 dark:text-zinc-300 dark:hover:text-white dark:hover:bg-zinc-900/65">
+    <button @click="changeTheme" title="Change theme" class="p-1.5 rounded-lg text-zinc-800 bg-transparent transition duration-150 hover:text-black hover:bg-zinc-100 dark:text-zinc-300 dark:hover:text-white dark:hover:bg-zinc-800">
         <span class="flex dark:hidden"><IconMoon :size="22" /></span>
-        <span class="hidden dark:block"><IconLight :size="23" /></span>
+        <span class="hidden dark:block"><IconLight :size="22" /></span>
     </button>
 </template>
