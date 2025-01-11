f615fdd020134d565d4ba35296a913922f190af6
esflowers <estrada.flsw@gmail.com>
2025-01-10 21:05:23 -0600

    change name layoutheader to theheader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-  import LayoutHeader from './layouts/LayoutHeader.vue';
+  import TheHeader from './layouts/TheHeader.vue';
 </script>
 
 <template>
-  <LayoutHeader />
+  <TheHeader />
   <main class="py-3 w-full h-1/2  flex items-center justify-center font-serif text-9xl text-center italic">
     write here Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro voluptatum possimus sint neque, asperiores nulla magnam consequuntur cupiditate odit dolore molestias eligendi hic praesentium laudantium quod dolores amet quam rerum!
   </main>
