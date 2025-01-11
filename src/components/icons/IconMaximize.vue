d72678e031ba979c92d63a0879c504aa38c5d311
esflowers <estrada.flsw@gmail.com>
2025-01-10 21:04:20 -0600

    create icons alert, check, maximize, minimize, and add lang ts

--- src/components/icons/IconMaximize.vue
+++ src/components/icons/IconMaximize.vue
@@ -17,8 +17,9 @@
     <svg  xmlns="http://www.w3.org/2000/svg"  :width="size"  :height="size"  
         viewBox="0 0 24 24"  fill="none"  
         stroke="currentColor"  :stroke-width="stroke"  stroke-linecap="round"  stroke-linejoin="round"  
-        class="icon icon-tabler icons-tabler-outline icon-tabler-moon">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+        class="icon icon-tabler icons-tabler-outline icon-tabler-maximize">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8v-2a2 2 0 0 1 2 -2h2" />
+        <path d="M4 16v2a2 2 0 0 0 2 2h2" /><path d="M16 4h2a2 2 0 0 1 2 2v2" />
+        <path d="M16 20h2a2 2 0 0 0 2 -2v-2" />
     </svg>
 </template>
--- src/components/icons/IconMinimize.vue
+++ src/components/icons/IconMinimize.vue
@@ -17,8 +17,9 @@
     <svg  xmlns="http://www.w3.org/2000/svg"  :width="size"  :height="size"  
         viewBox="0 0 24 24"  fill="none"  
         stroke="currentColor"  :stroke-width="stroke"  stroke-linecap="round"  stroke-linejoin="round"  
-        class="icon icon-tabler icons-tabler-outline icon-tabler-moon">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+        class="icon icon-tabler icons-tabler-outline icon-tabler-minimize">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 19v-2a2 2 0 0 1 2 -2h2" />
+        <path d="M15 5v2a2 2 0 0 0 2 2h2" /><path d="M5 15h2a2 2 0 0 1 2 2v2" />
+        <path d="M5 9h2a2 2 0 0 0 2 -2v-2" />
     </svg>
 </template>
