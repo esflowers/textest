cf6b5c2c4bf19a92b56788953225cb2ec858d215
esflowers <estrada.flsw@gmail.com>
2025-01-10 23:13:24 -0600

    create icon code

---
+++ src/components/icons/IconCode.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+    import { defineProps } from 'vue';
+
+    defineProps({
+        size: {
+            type: Number,
+            default: 24
+        },
+        stroke: {
+            type: Number,
+            default: 1.5
+        }
+    });
+</script>
+
+<template>
+    <svg  xmlns="http://www.w3.org/2000/svg"  :width="size"  :height="size"  
+        viewBox="0 0 24 24"  fill="none"  
+        stroke="currentColor"  :stroke-width="stroke"  stroke-linecap="round"  stroke-linejoin="round"  
+        class="icon icon-tabler icons-tabler-outline icon-tabler-code">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M7 8l-4 4l4 4" /><path d="M17 8l4 4l-4 4" />
+        <path d="M14 4l-4 16" />
+    </svg>
+</template>
