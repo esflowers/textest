f2d2eb106f530434def0ef5f2a628fd8906c2401
esflowers <estrada.flsw@gmail.com>
2025-01-11 16:18:24 -0600

    add component menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-  import TheHeader from './layouts/TheHeader.vue';
+  import TheHeader from '@/layouts/TheHeader.vue';
+  import TheMenu from '@/layouts/TheMenu.vue';
 </script>
 
 <template>
   <TheHeader />
-  <main class="py-3 w-full h-1/2  flex items-center justify-center font-serif text-9xl text-center italic">
+  <main class="w-full min-h-screen flex items-center justify-center font-serif text-9xl text-center italic">
     write here Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro voluptatum possimus sint neque, asperiores nulla magnam consequuntur cupiditate odit dolore molestias eligendi hic praesentium laudantium quod dolores amet quam rerum!
   </main>
+  <TheMenu />
 </template>
