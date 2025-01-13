dd572c4575235c1438c37d8f6aea24ae7df802dc
esflowers <estrada.flsw@gmail.com>
2025-01-12 19:08:48 -0600

    add basic content to the menu

--- src/layouts/TheMenu.vue
+++ src/layouts/TheMenu.vue
@@ -1,25 +1,60 @@
 <script setup lang="ts">
-
+    import InputSelect from '../components/inputs/InputSelect.vue';
+    import InputRange from '../components/inputs/InputRange.vue';
+    import InputCheck from '../components/inputs/InputCheck.vue';
+    import InputColor from '../components/inputs/InputColor.vue';
+    import BtnIcon from '../components/buttons/BtnIcon.vue';
+    import IconAlignLeft from '../components/icons/IconAlignLeft.vue';
+    import IconAlignCenter from '../components/icons/IconAlignCenter.vue';
+    import IconAlignRight from '../components/icons/IconAlignRight.vue';
+    import IconAlignJustify from '../components/icons/IconAlignJustify.vue';
+    import IconAlignStart from '../components/icons/IconAlignStart.vue';
+    import IconAlignEnd from '../components/icons/IconAlignEnd.vue';
 </script>
 
 <template>
-    <menu class="w-full h-fit fixed bottom-0 left-0 px-6 py-3 sm:px-8 sm:py-4 md:px-9 xl:px-12 2xl:py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-8 2xl:gap-4 bg-white dark:bg-zinc-950">
-        <div class="col flex flex-col gap-3">
-            <p>Font family</p>
-            <select name="fam" id="fam" class="bg-transparent p-1 px-3 rounded-lg border border-zinc-300 dark:border-zinc-800">
-                <option value="1">Geist</option>
-            </select>
-            <label for="customRange" class="form-label">Font size</label>
-            <input type="range" class="accent-black dark:accent-white h-3" id="customRange" >
-            <p>Font weight</p>
+    <menu class="w-fit h-screen overflow-auto fixed bottom-0 right-0 px-6 py-3 sm:px-8 sm:py-4 md:px-9 xl:px-12 2xl:py-5 flex flex-col gap-5 bg-white dark:bg-zinc-950">
+        <div class="col flex flex-col gap-5">
+            <InputSelect label="Font family" id="family" form="editText" />
+            <InputRange label="Font size" id="size" form="editText" />
+            <InputRange label="Font weight" id="weight" form="editText" />
         </div>
-        <div class="col flex flex-col gap-3">
-            <p>Font style</p>
-            <p>Letter spacing</p>
+        <div class="col flex flex-col gap-5">
+            <InputRange label="Letter spacing" id="tracking" unit="em" form="editText" />
+            <InputRange label="Line height" id="leading" unit="rem" form="editText" />
+            <InputColor label="Text color" id="color" form="editText" />
         </div>
-        <div class="col flex flex-col gap-3">
-            <p>Font variant numeric</p>
-
+        <div class="col flex flex-col gap-5">
+            <InputCheck label="Italic" id="italic" form="editText" />
+            <div class="flex flex-col gap-1">
+                <p class="select-none text-base">Text align</p>
+                <div class="flex items-center justify-start gap-2">
+                    <BtnIcon title="Left">
+                        <IconAlignLeft :size="20" />
+                    </BtnIcon>
+                    <BtnIcon title="Center">
+                        <IconAlignCenter :size="20" />
+                    </BtnIcon>
+                    <BtnIcon title="Right">
+                        <IconAlignRight :size="20" />
+                    </BtnIcon>
+                    <BtnIcon title="Justify">
+                        <IconAlignJustify :size="20" />
+                    </BtnIcon>
+                    <BtnIcon title="Start">
+                        <IconAlignStart :size="20" />
+                    </BtnIcon>
+                    <BtnIcon title="End">
+                        <IconAlignEnd :size="20" />
+                    </BtnIcon>
+                </div>
+            </div>
+            <InputSelect label="Text transform" id="transform" form="editText" />
+        </div>
+        <div class="col flex flex-col gap-5">
+            <InputSelect label="Font variant numeric" id="numeric" form="editText" />
+            <InputSelect label="Text decoration" id="decoration" form="editText" />
+            <InputSelect label="Text wrap" id="wrap" form="editText" />
         </div>
     </menu>
 </template>
