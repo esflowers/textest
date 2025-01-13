1ac4e008472fd52276c5dbc2832c46cec0cadf79
esflowers <estrada.flsw@gmail.com>
2025-01-12 19:07:35 -0600

    edit layout

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 
 <template>
   <TheHeader />
-  <main class="w-full min-h-screen flex items-center justify-center font-serif text-9xl text-center italic">
-    write here Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro voluptatum possimus sint neque, asperiores nulla magnam consequuntur cupiditate odit dolore molestias eligendi hic praesentium laudantium quod dolores amet quam rerum!
+  <main class="w-full h-full flex items-center justify-center font-serif text-9xl text-center italic">
+    write here
   </main>
   <TheMenu />
 </template>
