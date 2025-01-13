a0fb3c0776ee52d97f567d5e6b0a4f0e897d7df8
esflowers <estrada.flsw@gmail.com>
2025-01-13 16:26:33 -0600

    edit type props to primary button

--- src/components/buttons/BtnPrimary.vue
+++ src/components/buttons/BtnPrimary.vue
@@ -1,63 +1,43 @@
 <script setup lang="ts">
-    import { defineProps, defineEmits } from 'vue';
+    interface Props {
+        type: 'button' | 'submit' | 'reset',
+        text: string,
+        variable: 'primary' | 'secondary' | 'tertiary',
+        size: 'sm' | 'md' | 'lg',
+        isLink: boolean,
+        link: string,
+        target: '_blank' | '_self' | '_parent' | '_top',
+        disabled: boolean
+    }
 
-    defineProps({
-        type: {
-            type: String,
-            default: 'button'
-        },
-        text: {
-            type: String,
-            default: 'Click me'
-        },
-        variable: {
-            type: String,
-            default: 'primary'
-        },
-        size: {
-            type: String,
-            default: 'md'
-        },
-        isLink: {
-            type: Boolean,
-            default: false
-        },
-        link: {
-            type: String,
-            default: '#'
-        },
-        target: {
-            type: String,
-            default: '_blank'
-        },
-        disabled: {
-            type: Boolean,
-            default: false
-        }
+    const props = withDefaults(defineProps<Props>(), {
+        type: 'button',
+        text: 'Click me',
+        variable: 'primary',
+        size: 'md',
+        isLink: false,
+        link: '#',
+        target: '_blank',
+        disabled: false
     })
 
     const emit = defineEmits(['click']);
 
-    const getStyle = (variable, size, disabled) => {
+    const getStyle = (variable: string, size: string, disabled: boolean): string => {
         const base =  'px-3 py-1 w-fit h-fit rounded-lg flex items-center justify-center gap-1.5 font-semibold transition duration-150';
-        const variant = {
+        const variant: Record<'primary' | 'secondary' | 'tertiary', string> = {
             primary: 'text-zinc-100 bg-zinc-950 hover:text-white hover:bg-black dark:text-zinc-900 dark:bg-zinc-50 dark:hover:text-black dark:hover:bg-white',
-            secondary: 'text-zinc-900 bg-transparent border border-zinc-200 hover:text-black hover:bg-zinc-200 dark:border-zinc-700 dark:text-zinc-50 dark:hover:text-white dark:hover:bg-zinc-700',
+            secondary: 'text-zinc-900 bg-transparent border border-zinc-300 hover:text-black hover:bg-zinc-100 dark:border-zinc-800 dark:text-zinc-50 dark:hover:text-white dark:hover:bg-zinc-900',
             tertiary: 'text-zinc-800 bg-transparent hover:text-black hover:bg-zinc-100 dark:text-zinc-100 dark:hover:text-white dark:hover:bg-zinc-800'
         }
-        const sizeClass = {
+        const sizeClass: Record<'sm' | 'md' | 'lg', string> = {
             sm: 'text-sm',
             md: 'text-base',
             lg: 'text-lg'
         }
         const disabledClass = disabled ? 'opacity-80 cursor-not-allowed' : '';
 
-        return [
-            base,
-            variant[variable] || '',
-            sizeClass[size] || '',
-            disabledClass
-        ].join(' ');
+        return [base, variant[variable], sizeClass[size], disabledClass].join(' ');
     }
 </script>
 
