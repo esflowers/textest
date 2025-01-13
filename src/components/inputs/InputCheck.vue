6efa0e8348c1f38a0dd3ca63c32f39fa8303eaf5
esflowers <estrada.flsw@gmail.com>
2025-01-12 19:07:49 -0600

    create basic input check

---
+++ src/components/inputs/InputCheck.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import { defineProps } from 'vue';
+
+    defineProps({
+        label: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            required: true
+        },
+        form: {
+            type: String,
+            required: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    })
+</script>
+
+<template>
+    <label :for="id" :form="form" class="flex items-center gap-2 select-none text-base">
+        <input type="checkbox" :name="id" :id="id" :form="form" :disabled="disabled"
+            class="w-4 h-4 accent-zinc-900 dark:accent-zinc-200">
+        {{ label }}
+    </label>
+</template>
