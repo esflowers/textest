51c95ac0b259a8553b40718e12703283949121ae
esflowers <estrada.flsw@gmail.com>
2025-03-27 16:31:49 -0600

    create button size screen

---
+++ src/components/ui/ButtonSizeScreen.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import IconMaximize from '../icons/IconMaximize.vue';
+import IconMinimize from '../icons/IconMinimize.vue';
+import Button from '../Button.vue';
+
+const isFull = ref(false)
+
+const toggleFullScreen = () => {
+    if (!isFull.value) {
+        document.documentElement.requestFullscreen?.().then(() => {
+            isFull.value = true;
+        }).catch(err => console.error(err));
+    } else {
+        document.exitFullscreen?.().then(() => {
+            isFull.value = false;
+        }).catch(err => console.error(err));
+    }
+}
+</script>
+
+<template>
+    <Button variant="icon" title="Full screen" @click="toggleFullScreen">
+        <component :is="isFull ? IconMinimize : IconMaximize" :size="18" />
+    </Button>
+</template>
