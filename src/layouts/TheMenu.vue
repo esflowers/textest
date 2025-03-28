d511b5c3c08c01bb94c57a8941f0d7e48d6cdcb9
esflowers <estrada.flsw@gmail.com>
2025-03-27 19:36:55 -0600

    edit basic menu component

--- src/layouts/TheMenu.vue
+++ src/layouts/TheMenu.vue
@@ -1,70 +1,22 @@
 <script setup lang="ts">
-    import InputSelect from '../components/inputs/InputSelect.vue';
-    import InputRange from '../components/inputs/InputRange.vue';
-    import InputCheck from '../components/inputs/InputCheck.vue';
-    import InputColor from '../components/inputs/InputColor.vue';
-    import BtnIcon from '../components/buttons/BtnIcon.vue';
-    import IconAlignLeft from '../components/icons/IconAlignLeft.vue';
-    import IconAlignCenter from '../components/icons/IconAlignCenter.vue';
-    import IconAlignRight from '../components/icons/IconAlignRight.vue';
-    import IconAlignJustify from '../components/icons/IconAlignJustify.vue';
-    import IconAlignStart from '../components/icons/IconAlignStart.vue';
-    import IconAlignEnd from '../components/icons/IconAlignEnd.vue';
+import Button from '../components/Button.vue';
+import IconAlignCenter from '../components/icons/IconAlignCenter.vue';
+import IconFontSize from '../components/icons/IconFontSize.vue';
+import IconItalic from '../components/icons/IconItalic.vue';
+import IconBold from '../components/icons/IconBold.vue';
+import IconTypography from '../components/icons/IconTypography.vue';
+import IconBlockquote from '../components/icons/IconBlockquote.vue';
+import IconClearFormatting from '../components/icons/IconClearFormatting.vue';
 </script>
 
 <template>
-    <menu class="w-fit h-screen overflow-auto fixed top-14 sm:top-16 right-0 px-3 py-3 sm:px-4 sm:py-4 md:px-5 xl:px-6 2xl:py-5 flex flex-col gap-5 border-l border-zinc-300 bg-white dark:bg-zinc-950 dark:border-zinc-900">
-        <div class="col flex flex-col gap-5">
-            <InputSelect label="Font family" id="family" form="editText" />
-            <InputRange label="Font size" id="size" form="editText" />
-            <InputRange label="Font weight" id="weight" form="editText" />
-        </div>
-        <div class="col flex flex-col gap-5">
-            <InputRange label="Letter spacing" id="tracking" unit="em" form="editText" />
-            <InputRange label="Line height" id="leading" unit="rem" form="editText" />
-            <InputColor label="Text color" id="color" form="editText" />
-        </div>
-        <div class="col flex flex-col gap-5">
-            <InputCheck label="Italic" id="italic" form="editText" />
-            <div class="flex flex-col gap-1">
-                <p class="select-none text-base">Text align</p>
-                <div class="flex items-center justify-start gap-2">
-                    <BtnIcon title="Left">
-                        <IconAlignLeft :size="20" />
-                    </BtnIcon>
-                    <BtnIcon title="Center">
-                        <IconAlignCenter :size="20" />
-                    </BtnIcon>
-                    <BtnIcon title="Right">
-                        <IconAlignRight :size="20" />
-                    </BtnIcon>
-                    <BtnIcon title="Justify">
-                        <IconAlignJustify :size="20" />
-                    </BtnIcon>
-                    <BtnIcon title="Start">
-                        <IconAlignStart :size="20" />
-                    </BtnIcon>
-                    <BtnIcon title="End">
-                        <IconAlignEnd :size="20" />
-                    </BtnIcon>
-                </div>
-            </div>
-            <InputSelect label="Text transform" id="transform" form="editText" />
-        </div>
-        <div class="col flex flex-col gap-5">
-            <InputSelect label="Font variant numeric" id="numeric" form="editText" />
-            <InputSelect label="Text decoration" id="decoration" form="editText" />
-            <InputSelect label="Text wrap" id="wrap" form="editText" />
-        </div>
+    <menu class="w-fit h-fit hidden fixed bottom-6 left-1/2 -translate-x-1/2 rounded-full px-2.5 py-1.5 md:flex gap-1 backdrop-blur-md bg-white/90 border border-zinc-600">
+        <Button variant="menu" title="Font Size"><IconFontSize /></Button>
+        <Button variant="menu" title="Font Weight"><IconBold /></Button>
+        <Button variant="menu" title="Text"><IconBlockquote /></Button>
+        <Button variant="menu" title="Font"><IconTypography /></Button>
+        <Button variant="menu" title="Font Style"><IconItalic /></Button>
+        <Button variant="menu" title="Text Align"><IconAlignCenter /></Button>
+        <Button variant="menu" title="Clear Formatting"><IconClearFormatting /></Button>
     </menu>
 </template>
-
-<style scoped>
-    menu {
-        scrollbar-width: 8px !important;
-    }
-
-    ::-webkit-scrollbar {
-        width: 8px !important;
-    }
-</style>
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 export const appName = 'textest'
 export const author = 'esflowers'
-export const site = 'https://textest.esflowers.icu'
-export const repository = ''
+export const site = 'https://textest.vercel.app'
+export const repository = 'https://github.com/esflowers/textest'
