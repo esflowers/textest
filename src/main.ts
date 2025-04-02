5fecb605f3be87dd39d326b88615d4332cf112a6
esflowers <estrada.flsw@gmail.com>
2025-04-01 18:41:47 -0600

    edit info ts

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,3 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 const $ = (e: string) => document.querySelector(e);
-
-document.addEventListener('DOMContentLoaded', () => {
-    const currentTheme = localStorage.getItem('theme');
-
-    if (currentTheme === 'dark') {
-        document.documentElement.classList.add('dark')
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-})
