f7ca22e506482f5691be6b997389c72b27abd6d1
esflowers <estrada.flsw@gmail.com>
2025-04-04 17:16:21 -0600

    create font size component

---
+++ src/components/menu/FontSize.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { inject, Ref } from 'vue';
+import Button from '../Button.vue';
+import Dropdown from '../Dropdown.vue';
+import InputRange from '../inputs/InputRange.vue';
+import IconFontSize from '../icons/IconFontSize.vue';
+const fontSize = inject<Ref<number>>('fontSize')!;
+</script>
+
+<template>
+    <Dropdown position="-top-14 left-1/2 -translate-x-1/2">
+        <InputRange id="fontSize" v-model="fontSize" :min="12" :max="128" />
+
+        <template #trigger>
+            <Button variant="menu" title="Font Size">
+                <IconFontSize />
+            </Button>
+        </template>
+    </Dropdown>
+</template>
