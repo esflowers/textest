9d834ed62e64ce56a06b843bb529dc6acc412cfe
esflowers <estrada.flsw@gmail.com>
2025-04-04 16:27:13 -0600

    create dropdown component

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const isOpen = ref(false)
+const dropdownRef = ref<HTMLElement | null>(null)
+const toggle = () => (isOpen.value = !isOpen.value)
+
+interface Props {
+    position?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    position: '-top-16 left-1/2 -translate-x-1/2'
+})
+
+const handleClickOutside = (e: MouseEvent) => {
+    if (dropdownRef.value && !dropdownRef.value.contains(e.target as Node)) {
+        isOpen.value = false;
+    }
+}
+
+onMounted(() => {
+    document.addEventListener('click', handleClickOutside);
+})
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClickOutside);
+})
+</script>
+
+<template>
+    <div ref="dropdownRef" class="relative">
+        <transition name="fade">
+            <div v-if="isOpen"
+                :class="`absolute z-50 ${position} w-fit h-fit rounded-full px-2.5 py-1.5 flex gap-1 backdrop-blur-md bg-white/90 border border-zinc-600`">
+                <slot />
+            </div>
+        </transition>
+        <div @click="toggle">
+            <slot name="trigger" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 0.3s;
+    }
+
+    .fade-enter-from, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
