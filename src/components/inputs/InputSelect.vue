4dc935d1bfde45c217ac70907cf6f8cca18111fa
esflowers <estrada.flsw@gmail.com>
2025-04-04 22:43:13 -0600

    improve and fix input select

--- src/components/inputs/InputSelect.vue
+++ src/components/inputs/InputSelect.vue
@@ -1,36 +1,28 @@
 <script setup lang="ts">
-    import { defineProps } from 'vue';
+import Button from '../Button.vue';
+import Dropdown from '../Dropdown.vue'
+import IconChevronUp from '../icons/IconChevronUp.vue';
 
-    defineProps({
-        label: {
-            type: String,
-            required: true
-        },
-        id: {
-            type: String,
-            required: true
-        },
-        form: {
-            type: String,
-            required: true
-        },
-        disabled: {
-            type: Boolean,
-            default: false
-        }
-    })
+interface Props {
+    id: string,
+    options: string[],
+    default?: string,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    default: 'Select'
+})
 </script>
 
 <template>
-    <div class="flex flex-col gap-1">
-        <label :for="id" :form="form" class="select-none text-base">{{ label }}</label>
-        <select :id="id" :name="id" :form="form" :disabled="disabled"
-                class="py-1 px-2 rounded-lg font-medium text-base text-zinc-900 dark:text-zinc-50 bg-transparent border border-zinc-400 dark:border-zinc-800 focus:outline-none">
-            <option value="1" class="font-normal text-sm text-zinc-800 bg-white dark:text-zinc-400 dark:bg-zinc-950" selected>Lora</option>
-            <option value="2" class="font-normal text-sm text-zinc-800 bg-white dark:text-zinc-400 dark:bg-zinc-950">Poppins</option>
-            <option value="3" class="font-normal text-sm text-zinc-800 bg-white dark:text-zinc-400 dark:bg-zinc-950">Geist</option>
-            <option value="4" class="font-normal text-sm text-zinc-800 bg-white dark:text-zinc-400 dark:bg-zinc-950">Montserrat</option>
-            <option value="5" class="font-normal text-sm text-zinc-800 bg-white dark:text-zinc-400 dark:bg-zinc-950">Onest</option>
-        </select>
-    </div>
+    <Dropdown position="-top-36 left-1/2 -translate-x-1/2" class-name="flex-col" rounded="rounded-lg">
+        <p v-for="option in options" class="block w-[30ch] rounded-md font-medium tracking-tighter text-sm truncate cursor-pointer px-1 py-0.5 transition duration-150 hover:bg-zinc-300">{{ option }}</p>
+
+        <template #trigger>
+            <Button>
+                {{ default }}
+                <IconChevronUp />
+            </Button>
+        </template>
+    </Dropdown>
 </template>
