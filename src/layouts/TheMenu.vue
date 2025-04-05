ef1f44e813ae9517921c27caa7e9b5a7904918b4
esflowers <estrada.flsw@gmail.com>
2025-04-04 22:28:47 -0600

    replace new buttons

--- src/layouts/TheMenu.vue
+++ src/layouts/TheMenu.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import Button from '../components/Button.vue';
-import IconAlignCenter from '../components/icons/IconAlignCenter.vue';
-import IconFontSize from '../components/icons/IconFontSize.vue';
+import FontSize from '../components/menu/FontSize.vue';
+import TextSnippets from '../components/menu/TextSnippets.vue';
+import TextAlign from '../components/menu/TextAlign.vue';
 import IconItalic from '../components/icons/IconItalic.vue';
 import IconBold from '../components/icons/IconBold.vue';
 import IconTypography from '../components/icons/IconTypography.vue';
-import IconBlockquote from '../components/icons/IconBlockquote.vue';
 import IconClearFormatting from '../components/icons/IconClearFormatting.vue';
 </script>
 
 <template>
-    <menu class="w-fit h-fit hidden fixed bottom-6 left-1/2 -translate-x-1/2 rounded-full px-2.5 py-1.5 md:flex gap-1 backdrop-blur-md bg-white/90 border border-zinc-600">
-        <Button variant="menu" title="Font Size"><IconFontSize /></Button>
-        <Button variant="menu" title="Font Weight"><IconBold /></Button>
-        <Button variant="menu" title="Text"><IconBlockquote /></Button>
-        <Button variant="menu" title="Font"><IconTypography /></Button>
-        <Button variant="menu" title="Font Style"><IconItalic /></Button>
-        <Button variant="menu" title="Text Align"><IconAlignCenter /></Button>
-        <Button variant="menu" title="Clear Formatting"><IconClearFormatting /></Button>
+    <menu class="w-fit h-fit fixed bottom-3 left-1/2 -translate-x-1/2 rounded-full px-2.5 py-1.5 flex gap-1 backdrop-blur-md bg-white/90 border border-zinc-600">
+        <FontSize />
+        <Button variant="menu" title="Font Weight" :disabled="true"><IconBold /></Button>
+        <TextSnippets />
+        <Button variant="menu" title="Font" :disabled="true"><IconTypography /></Button>
+        <Button variant="menu" title="Font Style" :disabled="true"><IconItalic /></Button>
+        <TextAlign />
+        <Button variant="menu" title="Clear Formatting" :disabled="true"><IconClearFormatting /></Button>
     </menu>
 </template>
