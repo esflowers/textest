9e94165ced7070c4d337919a22c3e7414e6e0694
esflowers <estrada.flsw@gmail.com>
2025-04-13 21:48:43 -0600

    add color and top prop

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -6,13 +6,13 @@ const dropdownRef = ref<HTMLElement | null>(null)
 const toggle = () => (isOpen.value = !isOpen.value)
 
 interface Props {
-    position?: string,
+    top?: string,
     rounded?: string,
     className?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    position: '-top-16 left-1/2 -translate-x-1/2',
+    top: '-top-16',
     rounded: 'rounded-full',
     className: ''
 })
@@ -36,12 +36,12 @@ onUnmounted(() => {
     <div ref="dropdownRef" class="relative">
         <transition name="fade">
             <div v-if="isOpen"
-                :class="`absolute z-50 ${position} w-fit h-fit ${className} ${rounded} px-2.5 py-1.5 flex gap-1 backdrop-blur-md bg-white/90 border border-zinc-600`">
+                :class="`absolute z-50 ${top} left-1/2 -translate-x-1/2 w-fit h-fit ${className} ${rounded} px-2.5 py-1.5 flex gap-1 backdrop-blur-md bg-white/90 border border-zinc-600`">
                 <slot />
             </div>
         </transition>
         <div @click="toggle">
-            <slot name="trigger" />
+            <slot name="trigger" :is-open="isOpen" />
         </div>
     </div>
 </template>
