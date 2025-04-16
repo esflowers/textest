b28f83f57281b6f9efc8c0571a9a10c8be12c829
esflowers <estrada.flsw@gmail.com>
2025-04-15 22:08:57 -0600

    add new sgroup of snippets and add link to google fonts

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ export const appName = 'textest'
 export const author = 'esflowers'
 export const site = 'https://textest.esflowers.icu'
 export const repository = 'https://github.com/esflowers/textest'
+export const googleFonts = 'https://fonts.google.com/'
 export const snippets = [
     {
         name: 'Pangrams',
@@ -21,6 +22,17 @@ export const snippets = [
             "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        ]
+    },
+    {
+        name: 'Glyphs',
+        id: 'glyphs',
+        options: [
+            "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",
+            "abcdefghijklmnñopqrstuvwxyz",
+            "012345689",
+            ".,-_:;&@#*+|/'",
+            "(){}[]<>¡!¿?"
         ]
     }
 ]
