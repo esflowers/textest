0e5f8fb4646f3feff6a7f255ca2cd705bfefa0c3
esflowers <estrada.flsw@gmail.com>
2025-04-15 22:07:36 -0600

    create icon search

---
+++ src/components/icons/IconSearch.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { IconProps } from '../../types';
+
+const props = withDefaults(defineProps<IconProps>(), {
+    size: 21,
+    strokeWidth: 1.6,
+})
+</script>
+
+<template>
+    <svg  xmlns="http://www.w3.org/2000/svg"  :width="size"  :height="size"  
+        viewBox="0 0 24 24"  fill="none"  
+        stroke="currentColor"  :stroke-width="strokeWidth"  stroke-linecap="round"  stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+        <path d="M21 21l-6 -6" />
+    </svg>
+</template>
