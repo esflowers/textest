a040037d93addba1fe0567b3c83fd25a88da5524
esflowers <estrada.flsw@gmail.com>
2025-04-16 18:25:47 -0600

    create font family menu

---
+++ src/components/menu/FontFamily.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { googleFonts } from '../../utils';
+import Button from '../Button.vue';
+import Dropdown from '../Dropdown.vue';
+import InputText from '../inputs/InputText.vue';
+import IconTypography from '../icons/IconTypography.vue';
+import IconSearch from '../icons/IconSearch.vue';
+
+const props = defineProps<{ modelValue: string }>()
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <Dropdown top="-top-14" class-name="gap-3">
+        <Button variant="text-menu" :is-link="true" :link="googleFonts" class-name="text-xs" target="blank">From Google Fonts</Button>
+        <InputText id="searchFont" variant="transparent" placeholder="Font family name"
+                :modelValue="modelValue" @update:modelValue="emit('update:modelValue', $event)">
+            <IconSearch :size="16" />
+        </InputText>
+
+        <template #trigger="{ isOpen }">
+            <Button variant="menu" title="Typography" :class="isOpen ? 'text-blue-700' : ''">
+                <IconTypography />
+            </Button>
+        </template>
+    </Dropdown>
+</template>
