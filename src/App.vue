3dd9fd9a9156537ab429469412033fdc5e1213a2
esflowers <estrada.flsw@gmail.com>
2025-04-16 20:48:25 -0600

    add basic content for deploy

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted, provide } from 'vue';
+import { ref, onMounted, provide, watch } from 'vue';
 import type { TextAlign } from './types';
 import { appName } from './utils';
 import TheHeader from './layouts/TheHeader.vue';
 import TheMenu from './layouts/TheMenu.vue';
+import Toast from './components/Toast.vue';
 
-const font = ref('Lora');
+const font = ref('Geist Mono');
 const fontSize = ref(30);
 const textAlign = ref<TextAlign>('center');
 provide('fontSize', fontSize);
@@ -21,7 +22,21 @@ const loadFont = (fontName: string) => {
 }
 
 onMounted(() => {
-  loadFont(font.value);
+  loadFont(font.value)
+
+  const el = document.getElementById('textest')
+  if (!el) return;
+
+  el.addEventListener('paste', (e: ClipboardEvent) => {
+    e.preventDefault()
+    const text = e.clipboardData?.getData('text/plain')
+    if (!text) return;
+    document.execCommand('insertText', false, text)
+  })
+})
+
+watch(font, (newFont) => {
+  loadFont(newFont)
 })
 </script>
 
@@ -29,11 +44,14 @@ onMounted(() => {
   <TheHeader :font='font' />
   <main class="w-full min-h-dvh py-16 responsive-x flex items-center justify-center">
     <h1 id="textest" spellcheck="false" contenteditable="true" 
-        class="w-full outline-none list-disc"
+        class="w-full outline-none"
         :class="{'text-left': textAlign === 'left', 'text-center': textAlign === 'center', 'text-right': textAlign === 'right', 'text-justify': textAlign === 'justify'}"
         :style="{ fontFamily: font, fontSize: fontSize + 'px' }">
       {{ appName }}
     </h1>
   </main>
-  <TheMenu />
+  <TheMenu v-model:font="font" />
+  <Toast>
+    <p>This project is still in content development.</p>
+  </Toast>
 </template>
