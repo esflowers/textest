e2b8c5f779341849c588f909c3834d0cccd3675b
esflowers <estrada.flsw@gmail.com>
2025-04-29 20:47:05 -0600

    edit responsive button

--- src/components/ui/ButtonBuyMeACoffee.vue
+++ src/components/ui/ButtonBuyMeACoffee.vue
@@ -6,7 +6,7 @@ import IconClose from '../icons/IconClose.vue';
 
 const visible = ref(true)
 const position = computed(() => {
-    return visible.value ? 'left-1/2 -translate-x-1/2 top-4 md:top-5' : 'left-5 bottom-4 opacity-80'
+    return visible.value ? 'left-16 md:left-1/2 md:-translate-x-1/2 top-4 md:top-5' : 'left-5 bottom-4 opacity-80'
 })
 </script>
 
@@ -14,7 +14,7 @@ const position = computed(() => {
     <div role="alert" 
         :class="`w-fit h-fit z-50 fixed ${position} rounded-lg flex items-center gap-1.5 backdrop-blur-md bg-white/90 transition duration-150`">
         <a :href="buyMeACoffee" target="_blank">
-            <img class="w-28 lg:w-[115px] 2xl:w-32" src="../../assets/img/bmc-full-logo.svg" alt="Buy Me A Coffee">
+            <img class="w-24 sm:w-28 lg:w-[115px] 2xl:w-32" src="../../assets/img/bmc-full-logo.svg" alt="Buy Me A Coffee">
         </a>
         <span class="hidden md:flex">
             <Button v-if="visible" variant="menu" title="Close" @click="visible = false" >
