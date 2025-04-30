64abb2c4502cb2ae30319c78049db41785cf881e
esflowers <estrada.flsw@gmail.com>
2025-04-29 20:41:45 -0600

    create button of Buy me a coffee

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -10,7 +10,7 @@ const visible = ref(true)
     <div v-if="visible" role="alert" 
         class="w-[30ch] h-fit z-50 fixed bottom-3.5 right-3.5 rounded-lg px-2.5 py-1.5 font-medium text-sm tracking-tight flex items-center gap-2.5 backdrop-blur-md bg-white/90 border border-dashed border-zinc-800">
         <slot />
-        <Button target="blank" variant="menu" title="Close" @click="visible = false" >
+        <Button variant="menu" title="Close" @click="visible = false" >
             <IconClose :size="16" />
         </Button>
     </div>
---
+++ src/components/ui/ButtonBuyMeACoffee.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { buyMeACoffee } from '../../utils';
+import Button from '../Button.vue';
+import IconClose from '../icons/IconClose.vue';
+
+const visible = ref(true)
+const position = computed(() => {
+    return visible.value ? 'left-1/2 -translate-x-1/2 top-4 md:top-5' : 'left-5 bottom-4 opacity-80'
+})
+</script>
+
+<template>
+    <div role="alert" 
+        :class="`w-fit h-fit z-50 fixed ${position} rounded-lg flex items-center gap-1.5 backdrop-blur-md bg-white/90 transition duration-150`">
+        <a :href="buyMeACoffee" target="_blank">
+            <img class="w-28 lg:w-[115px] 2xl:w-32" src="../../assets/img/bmc-full-logo.svg" alt="Buy Me A Coffee">
+        </a>
+        <span class="hidden md:flex">
+            <Button v-if="visible" variant="menu" title="Close" @click="visible = false" >
+                <IconClose :size="16" />
+            </Button>
+        </span>
+    </div>
+</template>
